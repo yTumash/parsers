74ade50a4d674e1d76c0c3d1fa511cf5d19b6bde
yTumash <yanatumash2020@gmail.com>
2021-11-20 18:55:15 +0700

    Fourth commit

--- src/main/java/com/solvd/parsers/Main.java
+++ src/main/java/com/solvd/parsers/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         DomParser dom = new DomParser();
-        dom.parse(new File("src\\main\\resources\\booking.xml"));
+        dom.parse(new File("src/main/resources/booking.xml"));
         //JaxbParser jaxbparser = new JaxbParser();
-        //jaxbparser.parse(new File("src\main\resources\booking.xml"));
+        //jaxbparser.parse(new File("src/main/resources/booking.xml"));
         //JacksonParser jp = new JacksonParser();
-        //jp.parse(new File("src\main\resources\booking.json"));
+        //jp.parse(new File("src/main/resources/booking.json"));
     }
 }
